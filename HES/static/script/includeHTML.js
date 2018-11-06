639aeba8f78048ff1f79e542e2f03390788314b9
ysjk2003 <ysjk2003@gmail.com>
2018-11-06 16:57:29 +0900

    include

--- HES/static/script/includeHTML.js
+++ HES/static/script/includeHTML.js
@@ -3,6 +3,17 @@ function includeHTML() {
     z = document.getElementsByTagName("*");
     for (i = 0; i < z.length; i++) {
         elmnt = z[i]; file = elmnt.getAttribute("include-html");
-        if (file) { xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() { if (this.readyState == 4 && this.status == 200) { elmnt.innerHTML = this.responseText; elmnt.removeAttribute("include-html"); includeHTML();
-} } xhttp.open("GET", file, true); xhttp.send(); return; } } }
+        if (file) {
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    elmnt.innerHTML = this.responseText;
+                    elmnt.removeAttribute("include-html");
+                    includeHTML(); 
+                } 
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send(); return; 
+        } 
+    } 
+}
