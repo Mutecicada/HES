e4c4f37ee17a067d0587cba8cc016c3367ade6fe
jiung154 <37335831+jiung154@users.noreply.github.com>
2019-01-28 22:50:16 +0900

    database class modified

--- HES/DB.py
+++ HES/DB.py
@@ -10,13 +10,4 @@ class DataBase:
             db='HES',
             charset='utf8'
         )
-        self.cur = None
-
-    def get_cur(self):
         self.cur = self.db.cursor(pymysql.cursors.DictCursor)
-
-    def cmt(self):
-        self.db.commit()
-
-    def db_close(self):
-        self.db.close()
